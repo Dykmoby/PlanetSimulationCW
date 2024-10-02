6e2338c36649ec7c965bb1732d2af3af1080c279
Dykmoby <dovakin_lik@mail.ru>
2024-10-02 16:10:49 +0700

    First planet simulation

---
+++ PlanetSimulationCW/Model/Planet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetSimulationCW.Model
+{
+    class Planet
+    {
+        public Vector2 Position { get; private set; }
+        private Vector2 a;
+        public Vector2 Velocity { get; private set; }
+        public float Mass { get; private set; }
+        public float Radius { get; private set; }
+
+        public bool Destroyed { get; private set; }
+
+        public Planet(Vector2 position, float mass, float radius)
+        {
+            Position = position;
+            Mass = mass;
+            Radius = radius;
+            Velocity = new Vector2(0, 0);
+        }
+
+        public void Collide(Planet otherPlanet)
+        {
+            Mass += otherPlanet.Mass;
+            Velocity = Velocity + otherPlanet.Velocity * otherPlanet.Mass / Mass;
+            otherPlanet.Destroyed = true;
+        }
+
+        public void AddVelocity(Vector2 addVelocity)
+        {
+            Velocity += addVelocity;
+        }
+
+        public void SetVelocity(Vector2 setVelocity)
+        {
+            Velocity = setVelocity;
+        }
+
+        public void Move(float deltaTimeSquared)
+        {
+            Position += new Vector2(Velocity.X * deltaTimeSquared, Velocity.Y * deltaTimeSquared);
+        }
+    }
+}
---
+++ PlanetSimulationCW/Model/Simulation.cs
@@ -0,0 +1,84 @@
+﻿using System.Numerics;
+
+namespace PlanetSimulationCW.Model
+{
+    class Simulation
+    {
+        const double G = 6.6743015E-11F;
+        const int SPEED_OF_LIGHT = 299792458;
+        public List<Planet> planets;
+
+        // Время между кадрами (влияет на скорость симуляции). Пусть это будет постоянным значением пока что.
+        private float deltaTime = 0.16f;
+
+        public Simulation(int planetCount)
+        {
+            planets = new List<Planet>();
+            Random rand = new Random();
+
+            for (int i = 0; i < planetCount; i++)
+            {
+                Vector2 planetPosition = new Vector2(rand.Next(0, 500), rand.Next(0, 500));
+                float planetRadius = rand.Next(10, 40) / 10.0f;
+                float planetMass = (float) (Math.PI * planetRadius * planetRadius);
+
+                planets.Add(new Planet(planetPosition, planetMass, planetRadius));
+            }
+        }
+
+        public void SimulateStep()
+        {
+            float deltaTimeSquared = deltaTime * deltaTime;
+            for (int i = 0; i < planets.Count; i++)
+            {
+                if (planets[i].Destroyed)
+                {
+                    continue;
+                }
+                for (int k = 0; k < planets.Count; k++)
+                {
+                    if (i == k || planets[k].Destroyed)
+                    {
+                        continue;
+                    }
+
+                    Vector2 delta = planets[k].Position - planets[i].Position;
+
+                    float distanceSqr = Vector2.Dot(delta, delta);
+
+                    // Collision
+                    if (distanceSqr < (planets[i].Radius + planets[k].Radius) * (planets[i].Radius + planets[k].Radius))
+                    {
+                        if (planets[i].Mass > planets[k].Mass)
+                        {
+                            planets[i].Collide(planets[k]);
+                            continue;
+                        }
+                        else
+                        {
+                            planets[k].Collide(planets[i]);
+                            break;
+                        }
+                    }
+
+                    float invDistanceSqr = 1.0f / distanceSqr;
+                    Vector2 direction = Vector2.Normalize(delta);
+
+                    float velocity = (float)(10000000000 * G * planets[k].Mass * invDistanceSqr);
+
+                    planets[i].AddVelocity(direction * velocity);
+
+                    if (planets[i].Velocity.Length() >= SPEED_OF_LIGHT)
+                    {
+                        planets[i].SetVelocity(direction * (SPEED_OF_LIGHT - 100));
+                    }
+                }
+            }
+
+            for (int i = 0; i < planets.Count; i++)
+            {
+                planets[i].Move(deltaTimeSquared);
+            }
+        }
+    }
+}
